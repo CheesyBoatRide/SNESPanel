abec4fdf208d2a27494da2e806d40aa9d33d5990
CheesyBoatRide <cheesyboatride@gmail.com>
2024-04-27 20:51:41 -0500

    Add icon

--- build_win_portable.ps1
+++ build_win_portable.ps1
@@ -1,10 +1,10 @@
 npm run dist
-New-Item -Path .\build\stage
-New-Item -Path .\build\package
-Copy-Item -Path .\dist\*.exe -Destination .\build\stage -Force
-Copy-Item -Path .\config.toml -Destination .\build\stage -Force
+New-Item -Path . -Name \bin\stage -ItemType "directory"
+New-Item -Path . -Name .\bin\package -ItemType "directory"
+Copy-Item -Path .\dist\*.exe -Destination .\bin\stage -Force
+Copy-Item -Path .\config.toml -Destination .\bin\stage -Force
 $compress = @{
-  Path = ".\build\stage\*"
-  DestinationPath = ".\build\package\SNESControlPanel_Win64.zip"
+  Path = ".\bin\stage\*"
+  DestinationPath = ".\bin\package\SNESControlPanel_Win64.zip"
 }
 Compress-Archive @compress -Force
