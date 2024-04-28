9c37f3c1ed1394f82a79c868a8e9714c7c6cb24b
CheesyBoatRide <cheesyboatride@gmail.com>
2024-04-28 18:34:57 -0500

    Add files to release

--- build_win_portable.ps1
+++ build_win_portable.ps1
@@ -3,6 +3,9 @@ New-Item -Path . -Name \bin\stage -ItemType "directory"
 New-Item -Path . -Name .\bin\package -ItemType "directory"
 Copy-Item -Path .\dist\*.exe -Destination .\bin\stage -Force
 Copy-Item -Path .\config.toml -Destination .\bin\stage -Force
+Copy-Item -Path .\README.md -Destination .\bin\stage -Force
+Copy-Item -Path .\screenshot.png -Destination .\bin\stage -Force
+Copy-Item -Path .\LICENSE -Destination .\bin\stage -Force
 $compress = @{
   Path = ".\bin\stage\*"
   DestinationPath = ".\bin\package\SNESControlPanel_Win64.zip"
