227382ca4944554652d9543c4c2d614a5ac3034c
CheesyBoatRide <cheesyboatride@gmail.com>
2024-05-01 10:14:26 -0500

    New Icon

--- build_win_portable.ps1
+++ build_win_portable.ps1
@@ -1,8 +1,9 @@
 npm run dist
-New-Item -Path . -Name \bin\stage -ItemType "directory"
-New-Item -Path . -Name .\bin\package -ItemType "directory"
+New-Item -Path . -Name \bin\stage -ItemType "directory"  -Force
+New-Item -Path . -Name .\bin\package -ItemType "directory"  -Force
 Copy-Item -Path .\dist\*.exe -Destination .\bin\stage -Force
 Copy-Item -Path .\config.toml -Destination .\bin\stage -Force
+Copy-Item -Path .\config.toml -Destination .\dist\win-unpacked -Force
 Copy-Item -Path .\README.md -Destination .\bin\stage -Force
 Copy-Item -Path .\screenshot.png -Destination .\bin\stage -Force
 Copy-Item -Path .\LICENSE -Destination .\bin\stage -Force
