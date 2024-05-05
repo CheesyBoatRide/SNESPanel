86152c7b898de3d55bde4abf522015f0a7ae1eb1
CheesyBoatRide <cheesyboatride@gmail.com>
2024-05-05 16:07:49 -0500

    Whoops.

--- render/render.js
+++ render/render.js
@@ -44,18 +44,6 @@ function updateAppStatus(app, status) {
 
 ipcRenderer.on('initAppList', (_event, apps) => {
   for (const [key, value] of Object.entries(apps)) {
-<<<<<<< Updated upstream
-    let app_button = document.createElement("button");
-    app_button.id = value.display + "_button";
-    app_button.className = "appbutton";
-    app_button.addEventListener("click", () => {
-      toggleDynamicApp(value);
-    });
-
-    let drawer = document.getElementById("customAppDrawer");
-
-    drawer.appendChild(app_button);
-=======
     let drawer = document.getElementById("customAppDrawer");
     let app_button = document.getElementById(value.display + "_button");
     if (app_button === undefined || app_button === null) {
@@ -68,7 +56,6 @@ ipcRenderer.on('initAppList', (_event, apps) => {
     app_button.addEventListener("click", () => {
       toggleDynamicApp(value);
     });
->>>>>>> Stashed changes
 
     updateAppStatus(value.display, value.exists ? 'stopped' : 'error');
   }
@@ -78,7 +65,6 @@ ipcRenderer.on('updateAppStatus', (_event, app, status) => {
   updateAppStatus(app, status);
 })
 
-<<<<<<< Updated upstream
 
 ipcRenderer.on('initBrowserWindowList', (_event, pages) => {
   for (const [name, url] of Object.entries(pages)) {
@@ -101,63 +87,6 @@ ipcRenderer.on('initBrowserWindowList', (_event, pages) => {
     button.addEventListener("contextmenu", function () {
       embed.src = url;
     });
-=======
-ipcRenderer.on('initBrowserWindowList', (_event, pages) => {
-  for (const [name, url] of Object.entries(pages)) {
-    let button = document.createElement("button");
-    button.className = 'collapsible active';
-    button.textContent = name;
-    let section = document.createElement("section");
-    section.className = 'content';
-    let embed = document.createElement("EMBED");
-    embed.width = "100%";
-    embed.height = "600px";
-    embed.src = url;
-    embed.console = function () { };
-    section.appendChild(embed);
-    document.body.appendChild(button);
-    document.body.appendChild(section);
-
-    // refresh
-    button.addEventListener("contextmenu", function () {
-      embed.src = url;
-    });
-  }
-
-  const col = document.querySelectorAll(".collapsible");
-
-  for (let c of col) {
-    c.addEventListener("click", collapse);
-  }
-
-  function collapse(e) {
-    this.classList.toggle("active");
-    const content = this.nextElementSibling;
-    content.classList.toggle('hide');
-  }
-
-  const all = document.querySelector(".all");
-
-  all.addEventListener('click', collapseAll);
-
-  function collapseAll(e) {
-    const col = document.querySelectorAll('.collapsible');
-    const con = document.querySelectorAll('.content');
-
-    if (e.target.matches('.all')) {
-      this.classList.toggle("active");
-
-      col.forEach((button, index) => {
-        if (e.target.matches('.active')) {
-          button.classList.add('active');
-          con[index].classList.remove('hide');
-        } else {
-          button.classList.remove('active');
-          con[index].classList.add('hide');
-        }
-      });
-    }
->>>>>>> Stashed changes
   }
 })
 
